a05e87d2a2eb2677283d72a54473e31f13bb5cb2
TANMAY BISHNOI <tbishnoi@ryerson.ca>
2022-11-20 16:20:11 -0800

    ✅ fixed ros2 rgbd_odometry

--- ros2 usage/rtabmap/launch/README.md
+++ ros2 usage/rtabmap/launch/README.md
@@ -1,21 +1,27 @@
-launch file for launching realsense node with topic remapping done for rtabmap.
+# Using RGB-D odometry wirelessly using ROS2
+lorem ipsum dolor sit ameit
 
-Run this command in this dir to launch realsense camera node using launch file:
+## Instructions
+
+launch `realsense_node`
 ```
-ros2 launch launch_realsense/launch.py
+ros2 launch launch_realsense/launch.py approx_sync:=false
 ```
-For rtab-map:
+run `rgbd_odometry node`
 ```
-ros2 launch rtabmap_ros rtabmap.launch.py rtabmap_args:="--delete_db_on_start" frame_id:=camera_link
+ros2 run rtabmap_ros rgbd_odometry approx_sync:=false
+```
+run `rtabmap` server:
+```
+ros2 run rtabmap_ros rtabmap approx_sync:=false
 ```
 
-
-
-Rtabmap can be launched using:
+run `rtabmapviz` client:
 ```
-ros2 run rtabmap_ros rgbd_odometry
-ros2 run rtabmap_ros rtabmap approx_sync:=true rtabmapviz:=true rviz:=true rgbd_sync:=true
+ros2 run rtabmap_ros rtabmapviz
 ```
+
+## Tools
 
 Use `rqt_graph` to visualise remapping:
 ```
@@ -24,5 +30,9 @@ ros2 run rqt_graph rqt_graph
 
 Use `view_frames` to visualise tf frames:
 ```
-ros2 run tf2_tools view_frames
+ros2 run tf2_tools view_frames.py
+```
+## Misc.// To Be amended later
+```
+ros2 launch rtabmap_ros rtabmap.launch.py rtabmap_args:="--delete_db_on_start" frame_id:=camera_link
 ```
--- ros2 usage/rtabmap/launch/launch_realsense/launch.py
+++ ros2 usage/rtabmap/launch/launch_realsense/launch.py
@@ -27,102 +27,59 @@ realsense_camera_node (/camera)     R-TAB map param
                                     /rgb/camera_info
 
 
-Using Launch File for RTABMAP
+When Using Launch File for RTABMAP
                                     [rtabmap-2] rtabmap subscribed to (exact sync):
                                     [rtabmap-2]    /rtabmap/odom \
                                     [rtabmap-2]    /camera/rgb/image_rect_color \
                                     [rtabmap-2]    /camera/depth_registered/image_raw \
                                     [rtabmap-2]    /camera/rgb/camera_info \
                                     [rtabmap-2]    /rtabmap/odom_info
-
-
-
 '''
 
 def generate_launch_description():
     ## for run file
+    realsense_camera_node = Node(
+            package='realsense2_camera',
+            executable='realsense2_camera_node',
+            name='base',
+            remappings=[
+                ('/color/image_raw', '/rgb/image'),
+                ('/color/camera_info', '/rgb/camera_info'),
+                ('/depth/image_rect_raw', '/depth/image'),
+            ],
+            parameters=[
+                #{"base_frame_id": "link"},
+                #{"base_frame": "link"},
+                {"camera_name": "base"},
+                {"tf_publish_rate": 0.0},
+                # {"enable_gyro": "true"},
+                # {"enable_accel": "true"},
+
+            ]
+        )
+    
+
+    ## for rtabmap launch file
+
     # realsense_camera_node = Node(
     #         package='realsense2_camera',
     #         executable='realsense2_camera_node',
-    #         name='realsense_node',
+    #         name='base',
     #         remappings=[
-    #             ('/color/image_raw', '/rgb/image'),
-    #             ('/color/camera_info', '/rgb/camera_info'),
-    #             ('/depth/image_rect_raw', '/depth/image'),
+    #             ('/color/image_raw', '/camera/rgb/image_rect_color'),
+    #             ('/color/camera_info', '/camera/rgb/camera_info'),
+    #             ('/depth/image_rect_raw', '/camera/depth_registered/image_raw'),
     #         ],
     #         parameters=[
-    #             {"base_frame_id": "camera_link"},
-    #             {"base_frame": "camera_link"},
-    #             {"tf_publish_rate": 0.0}
+    #             #{"base_frame_id": "link"},
+    #             #{"base_frame": "link"},
+    #             {"camera_name": "base"},
+    #             {"tf_publish_rate": 0.0},
     #             # {"enable_gyro": "true"},
     #             # {"enable_accel": "true"},
 
     #         ]
     #     )
-    
-    ## for rtabmap launch file
-    
-    """
-    /accel/imu_info
-    /accel/metadata
-    /accel/sample
-    /color/metadata
-    /depth/camera_info
-    /depth/image
-    /depth/metadata
-    /gyro/imu_info
-    /gyro/metadata
-    /gyro/sample
-    /imu
-    /infra1/camera_info
-    /infra1/image_rect_raw
-    /infra1/metadata
-    /infra2/camera_info
-    /infra2/image_rect_raw
-    /infra2/metadata
-    /parameter_events
-    /rgb/camera_info
-    /rgb/image
-    /rosout
-    /tf_static
-    """
-    
-    realsense_camera_node = Node(
-            package='realsense2_camera',
-            executable='realsense2_camera_node',
-            name='realsense_node',
-            remappings=[
-                # [rtabmap-2]    /camera/rgb/image_rect_color \
-                # [rtabmap-2]    /camera/depth_registered/image_raw \
-                # [rtabmap-2]    /camera/rgb/camera_info \
-                # [rtabmap-2]    /rtabmap/odom_info
-                
-                ('/color/image_raw', '/camera/rgb/image_rect_color'),
-                ('/depth/image_rect_raw', '/camera/depth_registered/image_raw'),
-                ('/color/camera_info', '/camera/rgb/camera_info'),
-            ],
-            parameters=[
-                {"base_frame_id": "camera_link"},
-                {"base_frame": "camera_link"},
-                {"tf_publish_rate": 0.0}
-                # {"enable_gyro": "true"},
-                # {"enable_accel": "true"},
-            
-            ]
-        )
-    
-
-    # realsense_camera_node = Node(
-    #         package='realsense2_camera',
-    #         executable='realsense2_camera_node',
-    #         name='realsense_node',
-    #         remappings=[
-    #             ('/color/image_raw', '/camera/rgb/image_rect_color'),
-    #             ('/color/camera_info', '/camera/rgb/camera_info'),
-    #             ('/depth/image_rect_raw', '/camera/depth_registered/image_raw'),
-    #         ]
-    #     )
-
 
     return LaunchDescription([
         realsense_camera_node
