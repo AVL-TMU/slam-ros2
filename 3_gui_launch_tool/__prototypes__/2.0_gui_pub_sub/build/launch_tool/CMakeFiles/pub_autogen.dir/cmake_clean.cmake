0fb86e3ea9f6d827ef726dcbec88f8f6cc4c7852
TANMAY BISHNOI <tbishnoi@ryerson.ca>
2023-01-14 00:07:22 -0800

    untrack files in .gitignore

--- 3_gui_launch_tool/__prototypes__/2.0_gui_pub_sub/build/launch_tool/CMakeFiles/pub_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/pub_autogen"
-  "pub_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/pub_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- 3_gui_launch_tool/__prototypes__/2.0_gui_pub_sub/build/sub/CMakeFiles/sub_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/sub_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sub_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
